ca933a98b88b9114a557f4247e6b37215f5b4592
anyalebedenko <anyalebedenko@icloud.com>
2021-03-31 13:41:16 +0300

    Initial Commit

---
+++ Lebedenko_mobile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lebedenko_mobile
+//
+//  Created by Anna Lebedenko on 31.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
